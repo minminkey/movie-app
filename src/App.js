a5961df643c983648b442ff03cbde39c66e7f492
alsrl8259 <alsrl8259@gmail.com>
2020-12-22 16:53:55 +0900

    first day

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+import PropTypes from "prop-types";
+
+function Food({name, picture, rating}) {
+  return <div>
+    <h2>I like {name}</h2>
+    <h4>{rating}/5.0</h4>
+    <img src={picture} alt={name} />
+  </div>
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
+
+const foodIlike = [
+  {
+    id : 1,
+    name : "kimchi",
+    rating : 5,
+    image : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.OuwvJ-G_71I5bnUiylnN6AHaEK%26pid%3DApi&f=1"
+  },
+  {
+    id : 2,
+    name : "kimbap",
+    rating : 4.9,
+    image : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.OuwvJ-G_71I5bnUiylnN6AHaEK%26pid%3DApi&f=1"
+  },
+  {
+    id : 3,
+    name : "sam",
+    rating : 4.8,
+    image : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.OuwvJ-G_71I5bnUiylnN6AHaEK%26pid%3DApi&f=1"
+  },
+  {
+    id : 4,
+    name : "bol",
+    rating : 4.7,
+    image : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.OuwvJ-G_71I5bnUiylnN6AHaEK%26pid%3DApi&f=1"
+  }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div> 
+     {foodIlike.map(dish =>(
+        <Food 
+        key={dish.id}
+        name={dish.name}
+        picture = {dish.image}
+        rating = {dish.rating}
+        />
+     ))}
+  </div>
   );
 }
 
