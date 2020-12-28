e429123904ebfbe8dd91b93a8c2931550780c6c1
alsrl8259 <alsrl8259@gmail.com>
2020-12-28 20:58:28 +0900

    movie-app last

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,21 +3,21 @@ import {Link} from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({year, title, summary, poster, genres}){
+function Movie({id, year, title, summary, poster, genres}){
     return (
-      <Link
-        to={{
-          pathname: "/movie-detail",
-          state: {
-            year,
-            title,
-            summary,
-            poster,
-            genres,
-          },
-        }}
-      >
-        <div className="movie">
+      <div class="movie">
+        <Link
+          to={{
+            pathname: `/movie/${id}`,
+            state: {
+              year,
+              title,
+              summary,
+              poster,
+              genres,
+            },
+          }}
+        >
           <img src={poster} alt={title} title={title}></img>
           <div className="movie__date">
             <h3 className="movie__title">{title}</h3>
@@ -29,10 +29,10 @@ function Movie({year, title, summary, poster, genres}){
                 </li>
               ))}
             </ul>
-            <p className="movie_summary">{summary.slice(0, 140)}...</p>
+            <p className="movie_summary">{summary.slice(0, 180)}...</p>
           </div>
-        </div>
-      </Link>
+        </Link>
+      </div>
     );
 }
 
